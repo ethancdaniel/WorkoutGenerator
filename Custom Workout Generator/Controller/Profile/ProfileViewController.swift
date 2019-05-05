b20824ec8caf64859261faae1d04b86982d05732
Andrew <andrew.chang621@berkeley.edu>
2019-05-05 16:36:30 -0700

    made stats + saved workouts in profile

--- Custom Workout Generator/Controller/Profile/ProfileViewController.swift
+++ Custom Workout Generator/Controller/Profile/ProfileViewController.swift
@@ -11,4 +11,33 @@ import UIKit
 
 class ProfileViewController: UIViewController {
     
+    @IBOutlet weak var welcome: UILabel!
+    @IBOutlet weak var statsButton: UIButton!
+    @IBOutlet weak var workoutsButton: UIButton!
+    @IBOutlet weak var savedWorkouts: UIView!
+    @IBOutlet weak var personalStats: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        statsButton.isSelected = true
+        savedWorkouts.isHidden = true
+    }
+    
+    @IBAction func statsPressed(_ sender: Any) {
+        if !statsButton.isSelected {
+            statsButton.isSelected = true
+            workoutsButton.isSelected = false
+            personalStats.isHidden = false
+            savedWorkouts.isHidden = true
+        }
+    }
+    
+    @IBAction func workoutsPressed(_ sender: Any) {
+        if !workoutsButton.isSelected {
+            workoutsButton.isSelected = true
+            statsButton.isSelected = false
+            savedWorkouts.isHidden = false
+            personalStats.isHidden = true
+        }
+    }
 }
