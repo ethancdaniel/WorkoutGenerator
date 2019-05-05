8fb144c3586a2956e13d7494abc7d16aac9dbd1a
Ethan Daniel <ethandaniel@berkeley.edu>
2019-05-05 16:36:51 -0700

    Add saving workouts

--- Custom Workout Generator/Controller/Saved Workouts/SavedWorkoutsViewController.swift
+++ Custom Workout Generator/Controller/Saved Workouts/SavedWorkoutsViewController.swift
@@ -12,29 +12,43 @@ import Firebase
 class SavedWorkoutsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     var ref: DatabaseReference!
+    var savedWorkouts: [String:[Exercise]] = [:]
     @IBOutlet weak var savedWorkoutsTableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         savedWorkoutsTableView.dataSource = self
         savedWorkoutsTableView.delegate = self
-        ref = Database.database().reference()
+        
         if let currentUser = Auth.auth().currentUser {
-            let userWorkoutsRef = ref.child("Saved Workouts").child(currentUser.uid)
-            userWorkoutsRef.observeSingleEvent(of: .value) { (snapshot) in
-                for _ in 0 ..< snapshot.childrenCount{
-                    
+            ref = Database.database().reference().child("Saved Workouts").child(currentUser.uid)
+            ref.observe(DataEventType.value) { (snapshot) in
+                for child in snapshot.children.allObjects as! [DataSnapshot] {
+                    for exercise in child.children.allObjects as! [DataSnapshot] {
+                        let exerciseName = exercise.key
+                        let dict = exercise.value as? [String:AnyObject] ?? [:]
+                        let isCompound = dict["isCompound"]
+                        if let _ = self.savedWorkouts[child.key] {
+                            self.savedWorkouts[child.key]!.append(Exercise(name: exerciseName, parts: [], isCompound: isCompound as! Bool))
+                        } else {
+                            self.savedWorkouts[child.key] = [Exercise(name: exerciseName, parts: [], isCompound: isCompound as! Bool)]
+                        }
+                    }
                 }
+                self.savedWorkoutsTableView.reloadData()
             }
         }
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return savedWorkouts.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "workoutCell") as? WorkoutNamesTableViewCell {
+            cell.workoutName.text = Array(savedWorkouts.keys)[indexPath.row]
+            return cell
+        }
         return UITableViewCell()
     }
-
 }
---
+++ Custom Workout Generator/Controller/Saved Workouts/WorkoutNamesTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  WorkoutNamesTableViewCell.swift
+//  Custom Workout Generator
+//
+//  Created by Ethan Daniel on 5/5/19.
+//  Copyright © 2019 Ethan Daniel. All rights reserved.
+//
+
+import UIKit
+
+class WorkoutNamesTableViewCell: UITableViewCell {
+    @IBOutlet weak var workoutName: UILabel!
+}
