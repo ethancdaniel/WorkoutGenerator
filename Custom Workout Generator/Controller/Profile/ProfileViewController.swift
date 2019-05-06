481783af53b2e29d8da123e29bf2218ea8249e1a
Andrew <andrew.chang621@berkeley.edu>
2019-05-05 22:08:11 -0700

    meet

--- Custom Workout Generator/Controller/Profile/ProfileViewController.swift
+++ Custom Workout Generator/Controller/Profile/ProfileViewController.swift
@@ -10,6 +10,7 @@ import Foundation
 import UIKit
 import Firebase
 import Charts
+import ChameleonFramework
 
 class ProfileViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
